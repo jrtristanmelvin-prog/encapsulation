40d57d54e195b20111c1569ab7bf303454f191d0
Tristanjr <jrtristanmelvin@gmail.com>
2026-06-05 13:48:17 +0800

    add setter methods for fan attributes

--- fan-project/fan.py
+++ fan-project/fan.py
@@ -20,3 +20,15 @@ class Fan:
 
     def get_on(self):
         return self.__on
+    
+    def set_speed(self, speed):
+        self.__speed = speed
+
+    def set_radius(self, radius):
+        self.__radius = radius
+
+    def set_color(self, color):
+        self.__color = color
+
+    def set_on(self, on):
+        self.__on = on
